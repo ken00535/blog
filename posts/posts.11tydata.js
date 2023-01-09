a1501cdf81604223f169de6829afeaef0f5ae13a
kenwschen <ken00535@gmail.com>
2023-01-09 22:45:39 +0800

    feat: add home cover

--- posts/posts.11tydata.js
+++ posts/posts.11tydata.js
@@ -1,14 +1,17 @@
 module.exports = {
-  eleventyComputed: {
-      permalink: data => {
-          if(data.draft) return false;
-      },
-      tags: data => {
-          if(!data.draft) {
-            data.tags.push('posts');
-            return data.tags;
-          }
-          return [];
-      }
-  }
+    eleventyComputed: {
+        permalink: data => {
+            if (data.draft) return false;
+        },
+        tags: data => {
+            if (!data.draft) {
+                data.tags.push('posts');
+                return data.tags;
+            }
+            return [];
+        },
+        isPost: () => {
+            return true
+        }
+    }
 }
